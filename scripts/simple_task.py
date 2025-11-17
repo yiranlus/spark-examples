57e3f01d5e90909bf3f1f8b1ae0ee962d2c45efa
Yiran Lu <19369063+yiranlus@users.noreply.github.com>
2025-11-17 12:30:31 +0000

    Revert "refactor: use SparkSession"
    This reverts commit bfc9bc3628b287abb9a5f5589ee85f3abe5a8f1e.

--- scripts/simple_task.py
+++ scripts/simple_task.py
@@ -2,18 +2,16 @@
 A simple PySpark task that creates an RDD, performs a transformation,
 and collects the results.
 """
-from pyspark.sql import SparkSession
+from pyspark import SparkConf, SparkContext
 
-spark = (
-    SparkSession.builder
-    .appName("simple_task")
-    # .setMaster("local[*]")
-    .master("spark://51.91.85.24:7077")
-    # .set("spark.executor.cores", "1")
-    # .set("spark.executor.memory", "512m")
-    .getOrCreate()
+conf = (
+    SparkConf()
+    .setMaster("spark://51.91.85.24:7077")
+    .setAppName("simple_task")
+    .set("spark.executor.cores", "1")
+    .set("spark.executor.memory", "512m")
 )
-sc = spark.sparkContext
+sc = SparkContext(conf=conf)
 
 l = sc.parallelize(range(10))
 l_double = l.map(lambda x: x * 2)
